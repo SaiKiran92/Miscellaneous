69a08ef64866e9f9cc29d5ffa8d001f70d35661e
Sai Kiran Mayakuntla <SaiKiran92@users.noreply.github.com>
2020-10-30 15:37:47 +0530

    Add files via upload
    Importing SUMO's TraCI library in Julia.

---
+++ sumo_startup.jl
@@ -0,0 +1,14 @@
+using PyCall
+
+# telling PyCall to look for modules in 'tools' folder.
+tools = try
+			joinpath(ENV["SUMO_HOME"], "tools")
+		catch
+			@error "Please declare environmental variable 'SUMO_HOME'."
+		end
+pushfirst!(PyVector(pyimport("sys")."path"), tools)
+
+# importing modules
+traci = pyimport("traci")
+checkBinary = pyimport("sumolib")."checkBinary"
+
